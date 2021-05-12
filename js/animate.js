6da6d2b80bb23fe43894ac6c047c4b8c1eefb4c7
raassh <anggadwi2470@gmail.com>
2021-05-12 22:57:35 +0700

    Add files via upload

---
+++ js/animate.js
@@ -0,0 +1,58 @@
+function animate(typewriter) {
+    sequence = [
+        {
+            text: "Assalamu ‘alaikum wa rahmatullahi wa barakatuh,\n",
+            delayAfter: 500
+        },
+        {
+            text: "Saya Rangga Dwi Alamsyah mengucapkan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "Selamat Hari Raya Idul Fitri 1442 Hijriah.\n",
+            delayAfter: 500
+        },
+        {
+            text: "Serta di hari kemenangan, hari yang suci dan penuh berkah ini,\n",
+            delayAfter: 500
+        },
+        {
+            text: "jika saya pernah berbuat salah baik dari perilaku maupun...\n",
+            delayAfter: 500
+        },
+        {
+            text: "perkataan kepada kalian, maka ingatlah bahwa itu semua...\n",
+            delayAfter: 500
+        },
+        {
+            text: "MEMANG PANTAS UNTUK KALIAN!!!",
+            delayAfter: 1000
+        },
+        {
+            text: "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b adalah benar-benar kekhilafan saya.\n",
+            delayAfter: 500
+        },
+        {
+            text: "Oleh karena itu, saya atas nama pribadi juga ingin meminta...\n",
+            delayAfter: 500
+        },
+        {
+            text: "maaf sebesar-besarnya untuk kekhilafan-kekhilafan...\n",
+            delayAfter: 500
+        },
+        {
+            text: "yang pernah dilakukan.\n",
+            delayAfter: 500
+        },
+        {
+            text: "Minal Aidzin Wal Faidzin.\n",
+            delayAfter: 500
+        },
+        {
+            text: "Wassalamu 'alaikum wa rahmatullahi wa barakatuh.\n",
+            delayAfter: 500
+        },
+    ];
+
+    typewriter.playSequence(sequence);
+}
